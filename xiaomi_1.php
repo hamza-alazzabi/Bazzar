331af0a08719ccea6170e85b653c3684e850f257
Muhammed1997 <muhammed.lagha@gmail.com>
2019-02-10 20:23:57 +0200

    added xiaomi phone details pages

---
+++ xiaomi_1.php
@@ -0,0 +1,107 @@
+<?php
+require 'N&F.php';
+ ?>
+
+      <hr>
+			<hr>
+      <div class="container">
+      							<div class="col-lg">
+                      <h2>Xiaomi Mi 8 Pro</h2>
+      								<div class="thumbnail">
+      									<div class="row">
+      		 <img src="xiaomi\index-1.jpg"  alt="Responsive image">
+
+           <div class="col-sm" >
+           		<div class="img-thumbnail text-center">
+           		<img src="huawei\huawei-p-smart-2019-2.jpg" alt="My desktop">
+           				<div class="caption">
+           				<h5>Huawei <br>P Smart</h5>
+           				<!--p>
+           						New iPhone <br>
+           					  Get More Details
+           				</p-->
+           				<p>
+           				<a href="#" id="" class="btn btn-primary" role="button">more details</a></p>
+           				<!--<span><a href="#" class="btn btn-primary" role="button">By Now</a></span>-->
+           			</div>
+           		</div>
+           </div>
+
+      					 <table class="table">
+
+      				       <tr>
+      				         <th class="table-danger">NETWORK</th>
+      				         <th>Technology</th>
+      				         <th>GSM / HSPA / LTE</th>
+      				       </tr>
+
+      				     <tbody>
+
+      				       <tr>
+      				         <th class="table-danger">Primary</th>
+      				         <th class="table-primary">Announced <br>Status </th>
+      				         <th>2018, September <br>Available. Released 2018, September </th>
+      				       </tr>
+
+      				       <tr>
+      				         <th class="table-danger">BODY</th>
+      				         <th>Dimensions <br>Weight <br>Build <br>SIM <br>-- </th>
+      				         <th>154.9 x 74.8 x 7.6 mm (6.10 x 2.94 x 0.30 in)<br>177 g (6.24 oz)<br>Front/back glass, aluminum frame (7000 series)<br>Dual SIM (Nano-SIM, dual stand-by)</th>
+      				       </tr>
+
+      							 <tr>
+      				         <th class="table-danger">DISPLAY</th>
+      				         <th>Type <br>Size <br>Resolution <br>Protection <br>-- </th>
+      				         <th>LED-backlit IPS LCD, capacitive touchscreen, 16M colors
+      <br>4.7 inches, 60.9 cm2 (~65.6% screen-to-body ratio) <br>750 x 1334 pixels, 16:9 ratio (~326 ppi density) <br>Ion-strengthened glass, oleophobic coating <br>3D Touch</th>
+      				       </tr>
+
+      							 <tr>
+      				         <th class="table-danger">PLATFORM</th>
+      				         <th class="table-primary">Os <br>Chipset <br>CPU <br>GPU </th>
+      				         <th>Android 8.1 (Oreo); MIUI 10<br>Qualcomm SDM845 Snapdragon 845 (10 nm) <br>Octa-core (4x2.8 GHz Kryo 385 Gold & 4x1.8 GHz Kryo 385 Silver)<br>Adreno 630</th>
+      				       </tr>
+
+      							 <tr>
+      				         <th class="table-danger">MEMORY</th>
+      				         <th class="table-primary">Card slot <br>Internal </th>
+      				         <th>NO<br>128 GB, 6/8 GB RAM </th>
+      				       </tr>
+
+      							 <tr>
+      				         <th class="table-danger">MAIN CAMERA</th>
+      				         <th class="table-primary">Single <br>Features <br>Video </th>
+      				         <th>5 MP, f/2.2, 31mm (standard)<br>Face detection, HDR, panorama <br>1080p@30fps </th>
+      				       </tr>
+
+      							 <tr>
+      				         <th class="table-danger">COMMS</th>
+      				         <th>WLAN <br>Bluetooth <br>GPS <br>NFC <br>Radio <br>USB </th>
+      				         <th>Wi-Fi 802.11 a/b/g/n/ac, dual-band, hotspot<br>4.2, A2DP, LE <br>Yes, with A-GPS, GLONASS, GALILEO, QZSS <br>Yes (Apple Pay only) <br>NO <br>2.0, proprietary reversible connector </th>
+      				       </tr>
+
+      							 <tr>
+      				         <th class="table-danger">FEATURES</th>
+      				         <th>Sensors <br>-- </th>
+      				         <th>Fingerprint (front-mounted), accelerometer, gyro, proximity, compass, barometer <br>Siri natural language commands and dictation </th>
+      				       </tr>
+
+      							 <tr>
+      				         <th class="table-danger">BATTERY</th>
+      				         <th class="table-primary">-- <br>Stand-by <br>Talk time <br>Music play </th>
+      				         <th>Non-removable Li-Ion 1715 mAh battery (6.91 Wh) <br>Up to 240 h (3G) <br>Up to 14 h (3G) <br>Up to 50 h </th>
+      				       </tr>
+
+      				     </tbody>
+      				   </table>
+      </div>
+         </div>
+      </div>
+      </div>
+
+<?php
+require 'shops.php';
+require 'footer.php';
+ ?>
+			    </body>
+			</html>
